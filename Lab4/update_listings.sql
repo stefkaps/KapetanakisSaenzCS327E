3c2373c7fbbda4a0da0b185010a10be66dc33a67
DESKTOP-2IO1N0U\Stefanos <stefkaps@utexas.edu>
2018-02-16 22:01:14 -0600

    no message

--- Lab4/update_listings.sql
+++ Lab4/update_listings.sql
@@ -21,7 +21,7 @@ ALTER TABLE Listings ADD FOREIGN KEY (neighborhood, zipcode) REFERENCES Neighbor
 
 ALTER TABLE Summary_Listings RENAME COLUMN neighbourhood TO zipcode;
 
-UPDATE Listings SET street = split_part(street, ',', 1)
+UPDATE Listings SET street = split_part(street, ',', 1);
 
 UPDATE Listings SET price = REPLACE(price, ',', '');
 UPDATE Listings SET price = REPLACE(price, '$', '');
