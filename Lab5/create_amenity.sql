91c1074b9438e180a64b152e061373c7161e1bab
DESKTOP-2IO1N0U\Stefanos <stefkaps@utexas.edu>
2018-02-23 13:24:33 -0600

    no message

---
+++ Lab5/create_amenity.sql
@@ -0,0 +1,14 @@
+create table Amenity qas(
+   SELECT DISTINCT listing_id, amenities FROM Listing
+);
+
+ALTER TABLE Amenity ADD COLUMN amenity_name varchar(50);
+
+ALTER TABLE Amenity SET amenity_name = regexp_split_to_table(amenities, ',');
+ALTER TABLE Amenity DROP COLUMN amenities;
+
+ALTER TABLE Amenity ADD PRIMARY KEY (listing_id, amenity_name);
+
+ALTER TABLE Amenity ADD FOREIGN KEY (listing_id) REFERENCES Listing;
+
+ALTER TABLE Listing DROP COLUMN amenities;
--- Lab5/update_host.sql
+++ Lab5/update_host.sql
@@ -2,7 +2,13 @@ UPDATE Host SET response_rate = REPLACE(price, '%', '');
 UPDATE Host SET response_rate = REPLACE(price, 'N/A', NULL);
 ALTER TABLE Host ALTER COLUMN response_rate numeric;
 
-UPDATE Host SET city = split_part(location, ',', 1), state = split_part(location, ',', 2), country = split_part(location, ',', 3);
+ALTER TABLE Host ADD COLUMN city varchar(50), 
+ADD COLUMN state varchar(50), 
+ADD COLUMN country varchar(50);
+
+UPDATE Host SET city = split_part(location, ',', 1), 
+state = split_part(location, ',', 2), 
+country = split_part(location, ',', 3);
 
 ALTER TABLE Host DROP COLUMN location;
 
