a7df7387c654511acc2a9873391954c783c2d418
rsigns <rsaenz8080@gmail.com>
2018-02-23 21:36:42 -0600

    Update aggregate_queries.sql

--- Lab5/aggregate_queries.sql
+++ Lab5/aggregate_queries.sql
@@ -12,4 +12,26 @@ FROM Listing
 GROUP BY zipcode
 ORDER BY min(price) desc;
 
-#
+# Retrieves Host id and averages the response rate of hosts by state that are below 50%
+SELECT avg(h.response_rate), l.host_id
+FROM Host h JOIN Listing l ON l.host_id = h.id
+GROUP BY l.state
+HAVING avg(h.response_rate) <50;
+
+# Sums all prices of listings by date with prices > 0 
+SELECT c.date1, sum(l.price)
+FROM Calendar c JOIN Listing l ON c.listing_id = l.id
+GROUP BY c.date1
+HAVING sum(price) > 0;
+
+#Pulls the average 30 day availability for each host where 30 day availability is larger than 5
+SELECT l.host_id, avg(cs.availability_30) 
+FROM Calendar_summary cs JOIN Listing l ON cs.listing_id = l.id 
+WHERE cs.availability_30 > 5 
+GROUP BY l.host_id;
+
+#Averages the number of reviews by city per listing
+SELECT avg(number_of_reviews) 
+FROM Listing 
+WHERE number_of_reviews > 0 
+GROUP BY city;
