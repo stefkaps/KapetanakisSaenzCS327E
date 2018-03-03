52a6806fd2609ca9e912c9c30455e548fad48c3a
rsigns <rsaenz8080@gmail.com>
2018-03-02 21:29:03 -0600

    Update updated_aggregate_queries.sql

--- Lab6/updated_aggregate_queries.sql
+++ Lab6/updated_aggregate_queries.sql
@@ -3,26 +3,26 @@
 #Include all the listings and their average prices that dont have reviews.
 SELECT avg(l.price), r.reviewer_name, r.comments, l.id
 FROM Listing l LEFT OUTER JOIN Review r ON l.id = r.listing_id
-GROUP BY r.comments
-ORDER BY avg(price) desc;
+GROUP BY r.comments r.reviewer_name, l.id
+ORDER BY avg(price);
 
 #Lists airbnb listings id that have the lowest price by zipcode, and orders the results by price highest to lowest.
 SELECT min(price), id, zipcode
 FROM Listing
-GROUP BY zipcode
+GROUP BY zipcode, id
 ORDER BY min(price) desc;
 
-# Retrieves Host id and averages the response rate of hosts by state that are below 50%
+# Retrieves Host id and averages the response rate of hosts by state that are below 75%
 SELECT avg(h.response_rate), l.host_id
 FROM Host h JOIN Listing l ON l.host_id = h.id
 GROUP BY l.state
-HAVING avg(h.response_rate) <50;
+HAVING avg(h.response_rate) <75;
 
 # Sums all prices of listings by date with prices > 0 
 SELECT c.date1, sum(l.price)
 FROM Calendar c JOIN Listing l ON c.listing_id = l.id
 GROUP BY c.date1
-HAVING sum(price) > 0;
+HAVING sum(l.price) > 0;
 
 #Pulls the average 30 day availability for each host where 30 day availability is larger than 5
 SELECT l.host_id, avg(cs.availability_30) 
