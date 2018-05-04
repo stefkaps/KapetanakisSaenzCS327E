41bcecc0276016b706f2b6110de50817b63a04c0
DESKTOP-2IO1N0U\Stefanos <stefkaps@utexas.edu>
2018-05-04 11:37:53 -0500

    sql edit

--- Milestone 6/analysis.sql
+++ Milestone 6/analysis.sql
@@ -1,5 +1,8 @@
-#revenue generated over time and the # of listings
-SELECT 'Austin' as city, price, COUNT(listing_id)
+#revenue generated over time
+SELECT 'Austin' as city, price
 FROM `kapetanakissaenzcs327e.austin_temp.Listing`
 ORDER BY date;
 
+SELECT 'Austin' as city, COUNT(listing_id), date
+FROM `kapetanakissaenzcs327e.austin_temp.Listing`
+GROUP BY date;
