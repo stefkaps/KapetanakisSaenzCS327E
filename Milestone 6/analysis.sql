d4ef62021661982e74f8d410ca9372b45d25f8ef
rsigns <rsaenz8080@gmail.com>
2018-05-04 14:59:54 -0500

    Update analysis.sql

--- Milestone 6/analysis.sql
+++ Milestone 6/analysis.sql
@@ -1,5 +1,5 @@
 #revenue generated over time
-SELECT 'Austin' as city, l.price * l.minimum_nights * l.number_of_reviews as revenue
+SELECT 'Austin' as city, l.price * l.minimum_nights * l.number_of_reviews as revenue, c.date
 FROM `kapetanakissaenzcs327e.austin_temp.Listing` l JOIN `kapetanakissaenzcs327e.austin_temp.Calendar` c ON l.id = c.listing_id
 ORDER BY c.date;
 
