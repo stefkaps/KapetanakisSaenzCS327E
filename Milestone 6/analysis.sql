a29049ed30ab7f8e28a75f7f581df12a9d85a30e
DESKTOP-2IO1N0U\Stefanos <stefkaps@utexas.edu>
2018-05-04 14:51:22 -0500

    sql edit

--- Milestone 6/analysis.sql
+++ Milestone 6/analysis.sql
@@ -1,15 +1,15 @@
 #revenue generated over time
-SELECT 'Austin' as city, price
-FROM `kapetanakissaenzcs327e.austin_temp.Listing`
-ORDER BY date;
+SELECT 'Austin' as city, l.price
+FROM `kapetanakissaenzcs327e.austin_temp.Listing` l JOIN `kapetanakissaenzcs327e.austin_temp.Calendar` c ON l.id = c.listing_id
+ORDER BY c.date;
 
 #Number of rentals available grouped by the date
-SELECT 'Austin' as city, COUNT(listing_id), date
-FROM `kapetanakissaenzcs327e.austin_temp.Listing`
-GROUP BY date;
+SELECT 'Austin' as city, COUNT(l.id), c.date
+FROM `kapetanakissaenzcs327e.austin_temp.Listing` l JOIN `kapetanakissaenzcs327e.austin_temp.Calendar` c ON l.id = c.listing_id
+GROUP BY c.date;
 
 #occupancy rate over time
-SELECT 'Austin' as city, availability_365
-FROM `kapetanakissaenzcs327e.austin_temp.Listing`
-ORDER BY date;
+SELECT 'Austin' as city, l.minimum_nights * l.number_of_reviews as occupancy_rate
+FROM `kapetanakissaenzcs327e.austin_temp.Listing` l JOIN `kapetanakissaenzcs327e.austin_temp.Calendar` c ON l.id = c.listing_id
+ORDER BY c.date;
 
