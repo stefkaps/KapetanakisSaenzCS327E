95928fcfe6bfdc25ad1ee65323e06cd35401c9dc
DESKTOP-2IO1N0U\Stefanos <stefkaps@utexas.edu>
2018-05-04 15:19:12 -0500

    edits

--- Milestone 6/analysis.sql
+++ Milestone 6/analysis.sql
@@ -6,10 +6,12 @@ ORDER BY c.date;
 #Number of rentals available grouped by the date
 SELECT 'Austin' as city, COUNT(l.id) as num_of_rentals , c.date
 FROM `kapetanakissaenzcs327e.austin_temp.Listing` l JOIN `kapetanakissaenzcs327e.austin_temp.Calendar` c ON l.id = c.listing_id
-GROUP BY c.date;
+GROUP BY c.date
+ORDER BY c.date;
 
 #occupancy rate over time
 SELECT 'Austin' as city, l.minimum_nights * l.number_of_reviews as days_occupied
 FROM `kapetanakissaenzcs327e.austin_temp.Listing` l JOIN `kapetanakissaenzcs327e.austin_temp.Calendar` c ON l.id = c.listing_id
-ORDER BY c.date;
+ORDER BY c.date
+GROUP BY l.id;
 
