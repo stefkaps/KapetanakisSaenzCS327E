6047a6110d666060dfb2a5b715e7637ea5652069
DESKTOP-2IO1N0U\Stefanos <stefkaps@utexas.edu>
2018-05-04 14:25:29 -0500

    update sql

--- Milestone 6/analysis.sql
+++ Milestone 6/analysis.sql
@@ -3,6 +3,13 @@ SELECT 'Austin' as city, price
 FROM `kapetanakissaenzcs327e.austin_temp.Listing`
 ORDER BY date;
 
+#Number of rentals available grouped by the date
 SELECT 'Austin' as city, COUNT(listing_id), date
 FROM `kapetanakissaenzcs327e.austin_temp.Listing`
 GROUP BY date;
+
+#occupancy rate over time
+SELECT 'Austin' as city, availability_365
+FROM `kapetanakissaenzcs327e.austin_temp.Listing`
+ORDER BY date;
+
